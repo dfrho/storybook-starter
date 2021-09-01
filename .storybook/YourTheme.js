06fc606f4b21cd4c9672a577002b1e16a9583743
David Rhodes <davidrhodes@Davids-MBP.attlocal.net>
2021-09-01 15:14:43 -0500

    final commit

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,10 @@
+// .storybook/YourTheme.js
+
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "dark",
+  brandTitle: "My custom storybook",
+  brandImage:
+    "https://multichannelmerchant.com/wp-content/uploads/2020/02/shogun-logo-feature.jpg",
+});
